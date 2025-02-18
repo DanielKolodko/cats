1c45eac848c0d80a0785007ed2840458a5a91448
DanielKolodko <krek3r@gmail.com>
2025-02-18 04:17:15 +0200

    M

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,6 @@ provider "google" {
   region  = var.region
 }
 
-# Create a GKE cluster without the default node pool.
 resource "google_container_cluster" "primary" {
   name     = "cat-gifs-cluster"
   location = var.region
@@ -21,20 +20,13 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
-  network    = "default"
-  subnetwork = "default"
-
-  # Disable basic auth to encourage using secure credentials
   master_auth {
-    username = ""
-    password = ""
     client_certificate_config {
       issue_client_certificate = false
     }
   }
 }
 
-# Create a custom node pool using Ubuntu images.
 resource "google_container_node_pool" "primary_nodes" {
   name       = "cat-gifs-node-pool"
   cluster    = google_container_cluster.primary.name
@@ -45,7 +37,6 @@ resource "google_container_node_pool" "primary_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
-    # Specify Ubuntu as the node image type.
     image_type = "UBUNTU"
   }
 
