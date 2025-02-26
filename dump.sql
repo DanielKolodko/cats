810f6349b8bc78a56b603eeb09991cf2dee48154
DanielKolodko <krek3r@gmail.com>
2025-02-26 21:19:16 +0200

    M

---
+++ dump.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
+--
+-- Host: localhost    Database: catgifs
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cat_gifs`
+--
+
+DROP TABLE IF EXISTS `cat_gifs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cat_gifs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` varchar(500) NOT NULL,
+  `uploaded_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cat_gifs`
+--
+
+LOCK TABLES `cat_gifs` WRITE;
+/*!40000 ALTER TABLE `cat_gifs` DISABLE KEYS */;
+INSERT INTO `cat_gifs` VALUES (1,'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2p5aXpoaTZ6eDI5djFrcHRpc2VvY3A5OHIweXExbzZwbXl1NWM5eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o85xkQpyMlnBkpB9C/giphy.gif','2025-02-26 16:23:21'),(2,'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3dybjZ6YmZycW1qOTFhdjdxMG9yd3FzbzA1c3NsOG51NW02aXgyZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/mkmACvh5zomWc/giphy.gif','2025-02-26 16:24:10'),(3,'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExbjJrNmQxMzBjOTl4emNhM3VrNmJnOTUzanVuanVlYzM2ZWoxMGE0biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/rnkSmbEKJQ73W/giphy.gif','2025-02-26 16:24:55');
+/*!40000 ALTER TABLE `cat_gifs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-02-26 17:24:00
