f75e928bf843f89492d358ac8d8dae44da26fb97
DanielKolodko <krek3r@gmail.com>
2025-03-09 17:53:01 +0200

    M

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI ="mysql+pymysql://catuser:catpassword@mysql-service/catgifs"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'SQLALCHEMY_DATABASE_URI',
+        "mysql+pymysql://catuser:catpassword@mysql-service/catgifs"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "your_secret_key"
+    SECRET_KEY = os.environ.get('SECRET_KEY', "your_secret_key")
     SQLALCHEMY_ECHO = True  # This will log all SQL queries to the console
