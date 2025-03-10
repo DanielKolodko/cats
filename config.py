ab7e034f5eeb440e8fb7fa04f579fbccdaaf6cc8
DanielKolodko <krek3r@gmail.com>
2025-03-10 03:13:15 +0200

    M

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'SQLALCHEMY_DATABASE_URI',
-        "mysql+pymysql://catuser:catpassword@mysql-service/catgifs"
+        "mysql+pymysql://catuser:catpassword@my-release-helmchart-mysql-service/catgifs"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', "your_secret_key")
